c246b6d0fbc845c210df12a3207ac747f79e438a
Flameri <arto.lindgren1@gmail.com>
2017-09-21 15:11:22 +0300

    added

---
+++ conditional-statements/conditional-statment/conditional-statment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace conditional_statment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
