48335d62abbeaebdc6eae9336864e6f650c54750
Flameri <arto.lindgren1@gmail.com>
2017-09-21 15:39:23 +0300

    changes

--- conditional-statements/conditional-statment/conditional-statment/Program.cs
+++ conditional-statements/conditional-statment/conditional-statment/Program.cs
@@ -6,7 +6,17 @@ namespace conditional_statment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla");
+            Console.Write("Syötä luku: ");
+            string userInput; 
+            userInput =  Console.ReadLine();
+
+   
+            int.TryParse(userInput, out int evaluatedNumber);
+
+            Console.WriteLine("Syötit arvon {0}", userInput);
+            Console.WriteLine($"Syötit arvon { userInput}");
+            Console.ReadKey();
         }
     }
 }
