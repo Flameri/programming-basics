8e215919476c8cf89750f463b8d5ed3ef5ff8608
Flameri <arto.lindgren1@gmail.com>
2017-09-22 15:26:00 +0300

    jatkuu

---
+++ conditional-statements/conditional-statment/task - 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task___4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Syötä 3 lukua Ohjelma lajittelee luvut nousevaan järjestykseen.");
+            {
+                Console.Write("Syötä luku 1: ");
+                string userInput;
+                userInput = Console.ReadLine();
+
+                int number1.TryParse(user
+
+
+
+
+
+
+            }
+    }
+}
