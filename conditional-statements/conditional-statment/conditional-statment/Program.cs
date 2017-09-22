12df5092aed9b976c08268a9648fc756e5591337
Flameri <arto.lindgren1@gmail.com>
2017-09-22 13:01:20 +0300

    Done

--- conditional-statements/conditional-statment/conditional-statment/Program.cs
+++ conditional-statements/conditional-statment/conditional-statment/Program.cs
@@ -6,28 +6,42 @@ namespace conditional_statment
     {
         static void Main(string[] args)
         {
+            // Define instructions
             Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla");
-            Console.Write("Syötä luku: ");
-            string userInput;
-            userInput = Console.ReadLine();
-
-
-            int.TryParse(userInput, out int evaluatedNumber);
-
-            if (evaluatedNumber < 0)
+            bool isNumber;
+            do
             {
-                Console.WriteLine($"Numero {evaluatedNumber} on negatiivinen");
-            }
-            else if (evaluatedNumber > 0)
-            {
-                Console.WriteLine($"Numero {evaluatedNumber} on positiivinen");
-            }
-            else
-            {
-                Console.WriteLine($"Numero {evaluatedNumber} on nolla");
-            }
 
-            Console.WriteLine($"Syötit arvon {userInput}");    
+                Console.Write("Syötä luku: ");
+                //define variables
+                string userInput;
+                userInput = Console.ReadLine();
+
+
+                 isNumber = int.TryParse(userInput, out int evaluatedNumber);
+
+                // program logic
+                if (isNumber == true)
+                {
+                    if (evaluatedNumber < 0)
+                    {
+                        Console.WriteLine($"Numero {evaluatedNumber} on negatiivinen");
+                    }
+                    else if (evaluatedNumber > 0)
+                    {
+                        Console.WriteLine($"Numero {evaluatedNumber} on positiivinen");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Numero {evaluatedNumber} on nolla");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Syötit muuta kuin numeroita");
+                    Console.WriteLine($"Syötit arvon {userInput}");
+                }
+            } while (isNumber == false);
             Console.ReadKey();
         }
     }
