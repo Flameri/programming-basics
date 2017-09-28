9495de9cf93ff999911802d4194f440abec9cc28
flameri <arto.lindgren1@gmail.com>
2017-09-28 17:57:11 +0300

    Changes to 1.4

--- conditional-statements/conditional-statment-2/task - 4/Program.cs
+++ conditional-statements/conditional-statment-2/task - 4/Program.cs
@@ -10,19 +10,27 @@ namespace task___4
 
             Console.WriteLine("Syötä 3 lukua Ohjelma lajittelee luvut nousevaan järjestykseen.");
             {
+               
                 Console.Write("Syötä luku 1: ");
-                string userInput;
-                userInput = Console.ReadLine();
+                string num1;
+                num1 = Console.ReadLine();
 
-                int number1.TryParse(user
+                Console.Write("Syötä luku 2: ");
+                string num2;
+                num2 = Console.ReadLine();
+
+                Console.Write("Syötä luku 3: ");
+                string num3;
+                num3 = Console.ReadLine();
+               
+                
+
+
+                Console.ReadLine();
 
 
 
-
-
-
-
-
+                
             }
         }
     }
