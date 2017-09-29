e5faaae06a6fe563415392034f5be478e6f465ae
Flameri <arto.lindgren1@gmail.com>
2017-09-29 11:10:19 +0300

    1.5 Works

--- conditional-statements/conditional-statment-2/task - 4/Program.cs
+++ conditional-statements/conditional-statment-2/task - 4/Program.cs
@@ -8,30 +8,77 @@ namespace task___4
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            Console.WriteLine("Syötä 3 lukua Ohjelma lajittelee luvut nousevaan järjestykseen.");
+            Console.WriteLine("Syötä 3 lukua ohjelma lajittelee luvut nousevaan järjestykseen.");
+
+
+            Console.Write("Syötä luku 1: ");
+            string num1 = Console.ReadLine();
+            int numX = int.Parse(num1);
+
+
+            Console.Write("Syötä luku 2: ");
+            string num2 = Console.ReadLine();
+            int numY = int.Parse(num2);
+
+            Console.Write("Syötä luku 3: ");
+            string num3 = Console.ReadLine();
+            int numZ = int.Parse(num3);
+
+            if (numX < numY)
             {
-               
-                Console.Write("Syötä luku 1: ");
-                string num1;
-                num1 = Console.ReadLine();
+                if (numX < numZ)
+                {
+                    if (numY < numZ)
+                    {
+                        Console.WriteLine($"{numX}, {numY}, {numZ}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{numX}, {numZ}, {numY}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{numZ}, {numX}, {numY},");
+                }
 
-                Console.Write("Syötä luku 2: ");
-                string num2;
-                num2 = Console.ReadLine();
-
-                Console.Write("Syötä luku 3: ");
-                string num3;
-                num3 = Console.ReadLine();
-               
-                
-
-
-                Console.ReadLine();
-
-
-
-                
             }
+
+
+
+
+            else
+            {
+                if (numX < numZ)
+                {
+                    Console.WriteLine($"{numY},{numX},{numZ}");
+                }
+                else
+                {
+                    if (numY < numZ)
+                    {
+                        Console.WriteLine($"{numY},{numZ},{numX}");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine($"{numZ},{numY},{numX}");
+                    }
+                }
+  
+            }
+                     Console.ReadKey();
         }
     }
 }
+
+                
+
+
+
+
+
+                
+
+
+    
