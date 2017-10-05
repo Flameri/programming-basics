267deb0deb8f05ac0a76a7f9163175bf41588271
Flameri <arto.lindgren1@gmail.com>
2017-10-05 11:19:57 +0300

    New task

---
+++ conditional-statements/Task-2/Task-2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Ohjelma laskee lippusi hinnan antamiesi tietojen perusteella");
+            
+            Console.WriteLine("Syötä ikäsi: ");
+            string userInput 
+            userInput = Console.ReadLine();
+            Console.ReadLine();
+            bool isNumber;
+
+            isNumber = int.TryParse(userInput, out int age);            
+            
+            double price = 16;
+
+            double discount = 0;
+            
+            
+             //lasketaan ikä alennukset
+            if ( age < 7)
+            {
+                Console.WriteLine("Pääset ilmaiseksi");
+            }
+            else if (age >7 && age < 15)
+            {
+                discount = 0.5;
+            }
+           
+
+
+            
+
+
+
+
+            Console.WriteLine($"Lipun hintasi on: {price-price*discount}");
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ loop-statements/loop-example/loop-example/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace loop_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee kertoman. Anna luku, jonka kertoma lasketaan: ");
+            string userInput = Console.ReadLine();
+            //int number;
+            int number = int.Parse(userInput);
+            
+            //int.TryParse(userInput, out  int number);
+            int i = 0;
+            int f = 1;
+            do
+            {
+                
+                i = i + 1;
+              
+                f = f * i;
+                //Console.WriteLine($"{i}!= {f}"); // 5 != 120
+            } while (i < number);
+
+            Console.WriteLine($"Syötit {number}\n vastaus {f}"); // 5 != 120
+            Console.ReadKey();
+
+        }
+    }
+}
