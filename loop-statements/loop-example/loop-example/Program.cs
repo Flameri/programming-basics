e13bbabfc1bd9400458e35e39dfece91796bcf9b
Flameri <arto.lindgren1@gmail.com>
2017-10-06 18:33:23 +0300

    changes

--- loop-statements/loop-example/loop-example/Program.cs
+++ loop-statements/loop-example/loop-example/Program.cs
@@ -6,6 +6,8 @@ namespace loop_example
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             Console.WriteLine("Ohjelma laskee kertoman. Anna luku, jonka kertoma lasketaan: ");
             string userInput = Console.ReadLine();
             //int number;
@@ -14,15 +16,38 @@ namespace loop_example
             //int.TryParse(userInput, out  int number);
             int i = 0;
             int f = 1;
-            do
-            {
-                i = i + 1;
-              
-                f = f * i;
-                //Console.WriteLine($"{i}!= {f}"); // 5 != 120
-            } while (i < number);
 
-            Console.WriteLine($"Syötit {number}\n vastaus {f}"); // 5 != 120
+            if (number < 0)
+            {
+                Console.WriteLine("Virheellinen syöte");
+            }
+            else
+            {
+                /*
+                do
+                {
+                    i = i + 1;
+
+                    f = f * i;
+                    //Console.WriteLine($"{i}!= {f}"); // 5 != 120
+                } while (i < number);
+                */
+
+                /*
+                while (i < number)
+                {
+                    i = i + 1;
+                    f = f * i;
+                }
+                */
+                for(int  j=1;j <= number;j++)
+                {
+                    f = f * j;
+                    Console.WriteLine($"{j} != {f}");
+                }
+                Console.WriteLine($"Syötit {number}\n vastaus {f}"); // 5 != 120
+            }
+            
             Console.ReadKey();
 
         }
