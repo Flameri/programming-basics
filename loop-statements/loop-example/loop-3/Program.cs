b3c743877ff6e3bf50f2324b74645c3c23e97bac
Flameri <arto.lindgren1@gmail.com>
2017-10-12 08:41:27 +0300

    changes

--- loop-statements/loop-example/loop-3/Program.cs
+++ loop-statements/loop-example/loop-3/Program.cs
@@ -6,7 +6,32 @@ namespace loop_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Ohjelma laskee lisäyksen. Anna luku, jonka lisäys lasketaan: ");
+            string userInput = Console.ReadLine();
+            //int number;
+            int number = int.Parse(userInput);
+
+            //int.TryParse(userInput, out  int number);
+            int i = 1;
+            int f = 1;
+
+            if (number < 0)
+            {
+                Console.WriteLine("Virheellinen syöte");
+            }
+            else
+            {
+                do
+                {
+                    i = i + 1;
+
+                    f = f + i;
+                    Console.WriteLine($"{i}= {f}");
+                } while (i < number);
+            }
+            Console.ReadKey();
         }
     }
 }
