82285c86d5118c92895e0836b2c8207a84adc1fe
Flameri <arto.lindgren1@gmail.com>
2017-10-13 10:32:06 +0300

    Project 5 done

--- loop-statements/loop-example/loop-5/Program.cs
+++ loop-statements/loop-example/loop-5/Program.cs
@@ -15,8 +15,13 @@ namespace loop_5
 
             //int.TryParse(userInput, out  int number);
             int i = 0;
+            int f = 1;
             int evenSum = 0;
             int oddSum = 0;
+            if (number < 0)
+            {
+                f = -1;
+            }
             do
             {
                 if (i % 2 == 0)
@@ -26,9 +31,9 @@ namespace loop_5
 
                 i++;
             }
-            while (i <= number);
+            while (i <= number * f);
 
-            Console.WriteLine($"Parillisten lukujen summa = {evenSum} ja parittomien lukujen summa = {oddSum}");
+            Console.WriteLine($"Parillisten lukujen summa = {evenSum * f} ja parittomien lukujen summa = {oddSum * f}");
 
             Console.ReadKey();
         }
