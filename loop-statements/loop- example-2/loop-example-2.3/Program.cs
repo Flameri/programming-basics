1cb1628432c48a63deedda3fe562bbe1c0529777
Flameri <arto.lindgren1@gmail.com>
2017-10-20 09:16:43 +0300

    task 3 done

--- loop-statements/loop- example-2/loop-example-2.3/Program.cs
+++ loop-statements/loop- example-2/loop-example-2.3/Program.cs
@@ -6,9 +6,28 @@ namespace loop_example_2._3
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             Console.WriteLine("Ohjelma tulostaa 20 satunnaista kokonaislukua väliltä 0-50.");
             Random rnd = new Random();
 
+            for (int i = 0; i < 4; i++)
+            {
+                for (int k = 0; k < 5; k++)
+                {
+                    if (k < 4)
+                    {
+                        Console.Write($"{rnd.Next(50)}, ");
+                    }
+                    else
+                    {
+                        Console.WriteLine(rnd.Next(50));
+                    }
+                }
+            }
+            Console.ReadKey();
+
+            Console.ReadKey();
         }
     }
 }
