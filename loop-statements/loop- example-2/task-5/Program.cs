0e0d289db8fa921961986bf89d607b2e9f49480d
Flameri <arto.lindgren1@gmail.com>
2017-10-20 10:50:33 +0300

    progress task 5

--- loop-statements/loop- example-2/task-5/Program.cs
+++ loop-statements/loop- example-2/task-5/Program.cs
@@ -6,7 +6,20 @@ namespace task_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma tulostaa vakioveikkauksen rivin, 13 kohdetta X 1 tai 2");
+            Random rnd = new Random;
+           
+
+            for
+            {
+                if (x < 0.4)
+                    return "1";
+            }
+            
+
+
+            Console.ReadKey();
         }
     }
 }
