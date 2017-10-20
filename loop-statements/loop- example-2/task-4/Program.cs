0a72078d8f49f7c1a8f2e41be648fe58eac9a12a
Flameri <arto.lindgren1@gmail.com>
2017-10-20 09:54:42 +0300

    started task 4

---
+++ loop-statements/loop- example-2/task-4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Ohjelma simuloi rahan heittoa: ");
+            int.TryParse(Console.ReadLine(), out int n);
+            int kruuna = 1;
+            int klaava = 1;
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (rnd.Next(2) == 1)
+                {
+                    kruuna++;
+                }
+                else
+                {
+                    klaava++;
+                }
+                Console.WriteLine($"Kolikkoa on heitetty {n} kertaa.\n Klaavoja tuli {klaava} kruunia tuli {kruuna}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
