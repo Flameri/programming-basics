b576edc869e017b8049f99019680fa6f7f5902d2
Flameri <arto.lindgren1@gmail.com>
2017-10-23 09:57:08 +0300

    task 4.6 done

--- loop-statements/loop- example-2/task-6/Program.cs
+++ loop-statements/loop- example-2/task-6/Program.cs
@@ -6,7 +6,20 @@ namespace task_6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma heittää noppaa 1000 kertaa ja laskee kuutosen esiintymismäärän");
+            int x;
+            int y = 0;
+            Random rnd = new Random();
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                x = rnd.Next(1,7);
+                if (x == 6)
+                    y++;
+            }
+            Console.WriteLine($"Heitit noppaa 1000 kertaa, sait kutosen {y} kertaa");
+            Console.ReadKey();
         }
     }
 }
