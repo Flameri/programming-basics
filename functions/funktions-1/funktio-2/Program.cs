c22924977d297f21da77d2aeb03b184fd0441f7e
Flameri <arto.lindgren1@gmail.com>
2017-10-23 12:37:07 +0300

    changes

--- functions/funktions-1/funktio-2/Program.cs
+++ functions/funktions-1/funktio-2/Program.cs
@@ -7,7 +7,27 @@ namespace funktio_2
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Syötä ohjelmalle 2 numeroa, ohjelma kertoo kumpi on pienempi.");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            int x;
+            int y;
+            
+
+            int result = Calculator(x, y);
+        }
+        static int Calculator(int x,  int y)
+        {
+
+            if(x < y)
+            {
+                return x;
+            }
+            else
+            {
+                return y;
+            }
         }
     }
 }
