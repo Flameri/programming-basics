8253e55858177fc971a0bed2336c976683cee1b5
Flameri <arto.lindgren1@gmail.com>
2017-10-23 11:11:38 +0300

    funktio 1 done started 2

---
+++ functions/funktions-1/funktio-2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace funktio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- functions/funktions-1/funktions-1/Program.cs
+++ functions/funktions-1/funktions-1/Program.cs
@@ -6,7 +6,41 @@ namespace funktions_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma pyytää yhden luvun ja palauttaa luvun tähtinä");
+
+            int x;
+
+            while (true)
+            {
+                Console.WriteLine("Tähtien lukumäärä");
+                x = int.Parse(Console.ReadLine());
+
+                if (x < 0)
+                {
+                    Console.WriteLine("Luku {0} ei ole sallittu luku", x);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            string stars = StarMachine(x);
+            Console.WriteLine($" Syötit luvun {x}. Sait tähtiä {stars}");
+            Console.ReadKey();
+
+        }
+        static string StarMachine(int numberOfStarts)
+        {
+            string res = string.Empty;
+            for (int i = 0; i < numberOfStarts; i++)
+            {
+                res += "*";
+            }
+            return res;
+
         }
     }
 }
+
+
