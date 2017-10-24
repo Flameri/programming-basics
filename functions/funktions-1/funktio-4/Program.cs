c67a662bd1ed194d0e1aefb700d58f81b8e059a9
Flameri <arto.lindgren1@gmail.com>
2017-10-24 12:30:24 +0300

    started function 1.4

---
+++ functions/funktions-1/funktio-4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace funktio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
