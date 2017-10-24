79de55eac3c033618b76d9e7e311bcf83b577f18
Flameri <arto.lindgren1@gmail.com>
2017-10-24 12:19:30 +0300

    function 1.3 done

--- functions/funktions-1/funktio-3/Program.cs
+++ functions/funktions-1/funktio-3/Program.cs
@@ -6,7 +6,38 @@ namespace funktio_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            int retNumber = numberFromRange(1, 50);
+            Console.WriteLine($"Syöttämäsi luku {retNumber} on sallittu");
+            Console.ReadKey();
+
+        }
+        static int NumberFromRange(int lowerBound, int upperBound)
+        {
+            bool isNumber;
+            int input = 0;
+            int ret = 0;
+
+            do
+            {
+                Console.Write("Syötä numero väliltä 1-50: ");
+                isNumber = int.TryParse(Console.ReadLine(), out input);
+
+               
+                if (input >= lowerBound && input <= upperBound)
+                {
+                    ret = input;
+                    break;
+                }
+                else
+                {
+                    
+                    isNumber = false;
+                }
+            }
+            while (isNumber == false);
+
+            return ret;
         }
     }
 }
