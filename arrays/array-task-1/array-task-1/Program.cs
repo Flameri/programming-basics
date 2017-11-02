06f84c4acae778c8449a3007790edc6ae0f67aeb
Flameri <arto.lindgren1@gmail.com>
2017-11-02 13:36:47 +0200

    Array task-1 done

--- arrays/array-task-1/array-task-1/Program.cs
+++ arrays/array-task-1/array-task-1/Program.cs
@@ -6,7 +6,25 @@ namespace array_task_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma arpoo 100 lukua 0-50 väliltä ja ilmoittaa luvut ja niiden keskiarvon");
+            int[] iT = new int[100];
+            double sum = 0;
+            Random rnd = new Random();
+
+            for (int i = 0; i < iT.Length; i++)
+            {
+                iT[i] = rnd.Next(0, 50);
+                Console.WriteLine($"{i + 1}. {iT[i]}");
+            }
+            for (int i = 0; i < iT.Length; i++)
+            {
+                sum += iT[i];
+            }
+
+
+            Console.WriteLine($"{sum}  \n{sum/100}");
+            Console.ReadKey(); 
         }
     }
 }
