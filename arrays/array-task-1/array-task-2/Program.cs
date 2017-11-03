e263d3d212ff5cf14bf2fbd80a5fa7a01a906777
Flameri <arto.lindgren1@gmail.com>
2017-11-03 15:02:43 +0200

    array task 2 started

---
+++ arrays/array-task-1/array-task-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace array_task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma artoo 7 lukua 0-40 väliltä ja tulostaa ne pienimmästä suurimpaan");
+            int[] x = new int[7];
+            Random rnd = new Random();
+
+            for (int i = 0; i <= 6; i++)
+            {
+                x[i] = rnd.Next(1, 41);
+            }
+            Console.WriteLine($"Arvotut numerot ovat:");
+
+            Array.Sort(x);
+
+            foreach(int y in x)
+            {
+                Console.Write(y);
+                Console.Write(' ');
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
