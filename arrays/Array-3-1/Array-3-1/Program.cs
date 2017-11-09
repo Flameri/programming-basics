a35ca540a99c96211eb3390f47e684ca8f1e2d68
Flameri <arto.lindgren1@gmail.com>
2017-11-09 08:54:18 +0200

    Started arrays 3-1

---
+++ arrays/Array-3-1/Array-3-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Array_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+        }
+    }
+}
