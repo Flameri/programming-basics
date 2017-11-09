1426b48952b52f31b20ff4f018bfafdd6ae89fda
Flameri <arto.lindgren1@gmail.com>
2017-11-09 09:33:56 +0200

    Progress on array 3-1

--- arrays/Array-3-1/Array-3-1/Program.cs
+++ arrays/Array-3-1/Array-3-1/Program.cs
@@ -7,6 +7,16 @@ namespace Array_3_1
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma tulostaa 10:n numeron taulukkoon numerot 0-20 väliltä.");
+            int[] x = new int[10];
+            Random rnd = new Random();
+
+            for(int i = 0; i < x.Length; i++)
+            {
+                x[i] = rnd.Next(0, 20);
+                Console.WriteLine($"{x[i]}");
+            }
+            Console.ReadKey();
         }
     }
 }
