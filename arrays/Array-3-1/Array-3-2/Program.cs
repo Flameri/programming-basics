c00d4b6784907be87f2eb67882e7244988aa04be
Flameri <arto.lindgren1@gmail.com>
2017-11-10 10:34:23 +0200

    progress

--- arrays/Array-3-1/Array-3-2/Program.cs
+++ arrays/Array-3-1/Array-3-2/Program.cs
@@ -10,5 +10,19 @@ namespace Array_3_2
             Console.WriteLine("Ohjelma arpoo kokonaislukuja kaksiulotteiseen taulukkoon");
             int[,] = new int[0, 100];
         }
-    }
+        static string Sorter()
+        {
+            int y = rnd.Next(1, 21);
+            string res = "";
+
+            if (y < 10)
+            {
+                res = "0" + y.ToString();
+            }
+            else
+            {
+                res = y.ToString();
+            }
+            return res;
+        }
 }
