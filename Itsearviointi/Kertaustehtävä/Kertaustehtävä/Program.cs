75b9c60e39c195979d455b02d7cd76ed532c1b14
Flameri <arto.lindgren1@gmail.com>
2017-11-10 08:35:39 +0200

    started kertaustehtävä

---
+++ Itsearviointi/Kertaustehtävä/Kertaustehtävä/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Kertaustehtävä
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma tulostaa annetun määrän tähtiä");
+            int[] numbers = new int[{ 2, 5, 10, 7, 3 }];
+
+           for(int i = 0; i < numbers.Length; i++)
+            {
+
+            }
+
+        }
+        static string StarMachine(int stars)
+        {
+            string res = string.Empty;
+
+            for(int i= 0; i < stars; i++)
+            {
+                res += "*";
+            }
+            return res;
+
+
+        }
+    }
+}
