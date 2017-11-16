dee0c52fbfb0164f1406124b493916af5c02176d
Flameri <arto.lindgren1@gmail.com>
2017-11-16 14:43:43 +0200

    kertaus 2 done

--- Itsearviointi/Kertaustehtävä/kertaustehtävät-16-11/Kertaustehtävä-2/Program.cs
+++ Itsearviointi/Kertaustehtävä/kertaustehtävät-16-11/Kertaustehtävä-2/Program.cs
@@ -9,10 +9,33 @@ namespace Kertaustehtävä_2
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("Minäpä tiedän luvun väliltä 1-00 jota sinä et tiedä!");
             Random rnd = new Random();
-            int x = rnd.Next(1 - 100);
+            int x = rnd.Next(1,101);
             int guess = 0;
+            int y;
 
+            do
+            {
+                Console.WriteLine("Arvaa luku:");
+                string userInput = Console.ReadLine();
+                y = int.Parse(userInput);
+                guess++;
 
+                if (x == y)
+                {
+                    Console.WriteLine($"Arvasit oikein! Arvasit {guess} kertaa");
+                }
+                else if (x < y)
+                {
+                    Console.WriteLine("Lukuni on pienempi");
+                }
+                else if (x > y)
+                {
+                    Console.WriteLine("Lukuni on suurempi");
+                }
+            } while (x != y);
+
+           
+            Console.ReadLine();
         }
     }
 }
