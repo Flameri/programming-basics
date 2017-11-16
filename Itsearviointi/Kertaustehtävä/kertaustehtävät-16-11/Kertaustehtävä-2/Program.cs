77d2f7c4de4fcfeb0cf2f54aa60afaf4a2976581
Flameri <arto.lindgren1@gmail.com>
2017-11-16 13:32:34 +0200

    started kertaustehtävät

---
+++ Itsearviointi/Kertaustehtävä/kertaustehtävät-16-11/Kertaustehtävä-2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Kertaustehtävä_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Minäpä tiedän luvun väliltä 1-00 jota sinä et tiedä!");
+            Random rnd = new Random;
+        }
+    }
+}
---
+++ Itsearviointi/Kertaustehtävä/kertaustehtävät-16-11/kertaustehtävät-16-11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace kertaustehtävät_16_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma kysyy lukuja, kunnes syötät kaksi samaa.");
+            bool number;
+
+            do
+            {
+                Console.WriteLine("Syötä luku:");
+                string userInput = Console.ReadLine();
+                int x = int.Parse(userInput);
+                number = int.TryParse(userInput, out int y);
+
+                if(y == y)
+                {
+                    Console.WriteLine("Ohjelman suoritus päättyy");
+                    break;
+                }
+                else if(y != y)
+                {
+                    y++;
+                }
+
+
+            } while (true);
+
+
+            Console.WriteLine($"");
+            Console.ReadLine();
+        }
+    }
+}
