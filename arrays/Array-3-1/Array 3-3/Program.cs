f7f83e0f0064c0da039fda1386c1c374e94af03a
Flameri <arto.lindgren1@gmail.com>
2017-11-17 13:58:35 +0200

    started array 3-3

---
+++ arrays/Array-3-1/Array 3-3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Array_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
