f9681638c7b070975160a898fbede0124a78f4af
Flameri <arto.lindgren1@gmail.com>
2017-11-17 13:33:03 +0200

    progress array 3-2

--- arrays/Array-3-1/Array-3-2/Program.cs
+++ arrays/Array-3-1/Array-3-2/Program.cs
@@ -4,22 +4,32 @@ namespace Array_3_2
 {
     class Program
     {
+        //Ohjelma oli liian nopea oli määriteltävä rnd ylemmäs
+        static Random rnd = new Random();
+        
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("Ohjelma arpoo kokonaislukuja kaksiulotteiseen taulukkoon");
-            int[,] arr = new int[0, 100];
+            int[,] arr = new int[10, 20];
 
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i <= 10; i++)
             {
-               arr[i,i] = Sorter();
-                Console.WriteLine($"{i + 1} = {arr[i]}");
+                for (int j = 0; i <= 20; j++)
+                {
+                    if (arr[i,j] < 10)
+                    {
+
+
+
+                    }
+                }
             }
             Console.ReadKey();
         }
         static string Sorter()
         {
-            int y = rnd.Next(1, 21);
+            int y = rnd.Next(0, 100);
             string res = "";
 
             if (y < 10)
