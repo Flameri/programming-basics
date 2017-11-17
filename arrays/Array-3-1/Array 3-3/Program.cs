ef1a0147f5829fcbc208f98e1a1186c9b5752b42
Flameri <arto.lindgren1@gmail.com>
2017-11-17 14:53:29 +0200

    progress

--- arrays/Array-3-1/Array 3-3/Program.cs
+++ arrays/Array-3-1/Array 3-3/Program.cs
@@ -7,7 +7,28 @@ namespace Array_3_3
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma määrittelee kolme taulukkoa, joissa on jokaisessa 10 lukua.");
+
+            int[] arrT_1 = new int[10];
+            int[] arrT_2 = new int[10];
+            int[] arrT_3 = new int[10];
+
+            Random rnd = new Random();
+            
+
+            for (int i = 0; i < 10; i++)
+            {
+                arrT_1[i] = rnd.Next(0, 10);
+                Console.Write($"{i + 1}  {arrT_1[i]}\n");
+            }
+            for (int j = 0; j < 10; j++)
+            {
+                arrT_2[j] = rnd.Next(0, 10);
+                Console.Write($" {arrT_2[j]} ");
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
