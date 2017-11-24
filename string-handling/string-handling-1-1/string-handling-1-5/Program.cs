de01c9739bd6b1485800aba5878be6410766cad6
Flameri <arto.lindgren1@gmail.com>
2017-11-24 09:43:41 +0200

    Started string handling 1-5

---
+++ string-handling/string-handling-1-1/string-handling-1-5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace string_handling_1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma tarkistaa onko syöttämäsi teksti palindromi");
+            string userInput = Console.WriteLine().ToUpper();
+
+
+        }
+    }
+}
