d532a843ffd3331db58acd0eeeeb00c93c16e3e5
Flameri <arto.lindgren1@gmail.com>
2017-11-24 11:04:04 +0200

    progress string handling 2-1

--- string-handling/string-handling-2-1/string-handling-2-1/Program.cs
+++ string-handling/string-handling-2-1/string-handling-2-1/Program.cs
@@ -9,7 +9,19 @@ namespace string_handling_2_1
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("Ohjelma maksaa vokaalien määrän");
             string userInput = Console.ReadLine().ToUpper();
+            string[] arr1 = new string[] { "a,e,i,o,u,y,ä,ö" };
+            int i = 0;
 
-        }
+            foreach(char x in userInput)
+            {
+                if (x == arr1[])
+                    i++;
+                    
+              
+                
+            }
+
+            Console.ReadKey();
+        } 
     }
 }
