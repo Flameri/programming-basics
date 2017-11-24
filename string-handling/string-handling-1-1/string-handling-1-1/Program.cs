9806c3e09291ab6ba6723da1307d93f4999e38a0
Flameri <arto.lindgren1@gmail.com>
2017-11-24 08:36:09 +0200

    started task string handling

---
+++ string-handling/string-handling-1-1/string-handling-1-1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace string_handling_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
