16b7f71bc34eeaa55fe87fd341773358d87e6e22
Flameri <arto.lindgren1@gmail.com>
2017-11-24 09:40:54 +0200

    string-handling 1-4 done

--- string-handling/string-handling-1-1/string-handling-1-4/Program.cs
+++ string-handling/string-handling-1-1/string-handling-1-4/Program.cs
@@ -6,7 +6,19 @@ namespace string_handling_1_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Syötä tekstiä");
+            string userInput = Console.ReadLine().ToUpper();
+
+            int j = 0;
+            foreach(char x in userInput) 
+            {
+                if (x == 'L')
+                    j++;
+            }
+
+            Console.WriteLine($"Tekstissäsi on {j} L- kirjainta");
+            Console.ReadKey();
         }
     }
 }
