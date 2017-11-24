a6d97c463f2201ca417933d6fdd575e7dfd2b44d
Flameri <arto.lindgren1@gmail.com>
2017-11-24 10:44:57 +0200

    string handling 2-1

---
+++ string-handling/string-handling-2-1/string-handling-2-1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace string_handling_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma maksaa vokaalien määrän");
+            string userInput = Console.ReadLine().ToUpper();
+
+        }
+    }
+}
