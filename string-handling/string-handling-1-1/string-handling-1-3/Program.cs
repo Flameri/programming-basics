da5daec100e267bd7fc08a1e1ebf27431cd5a770
Flameri <arto.lindgren1@gmail.com>
2017-11-24 09:09:19 +0200

    string handling 1-3 done

--- string-handling/string-handling-1-1/string-handling-1-3/Program.cs
+++ string-handling/string-handling-1-1/string-handling-1-3/Program.cs
@@ -6,7 +6,13 @@ namespace string_handling_1_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Syötä tekstiä");
+            string userInput = Console.ReadLine();
+            userInput.Replace("e", "@");
+
+            Console.WriteLine($"Syötit tekstin: {userInput.Replace("e", "@")}");
+            Console.ReadKey();
         }
     }
 }
