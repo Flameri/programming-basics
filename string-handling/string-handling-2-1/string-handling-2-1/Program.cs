95014f36c54bbc4a4229a091c0e757e888da585f
Flameri <arto.lindgren1@gmail.com>
2017-12-07 12:44:34 +0200

    string handling 2-1 done

--- string-handling/string-handling-2-1/string-handling-2-1/Program.cs
+++ string-handling/string-handling-2-1/string-handling-2-1/Program.cs
@@ -7,20 +7,22 @@ namespace string_handling_2_1
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.WriteLine("Ohjelma maksaa vokaalien määrän");
+            Console.WriteLine("Ohjelma laskee vokaalien määrän");
             string userInput = Console.ReadLine().ToUpper();
-            string[] arr1 = new string[] { "a,e,i,o,u,y,ä,ö" };
-            int i = 0;
+            char[] arr1 = new char[] {'A','E','I','O','U','Y','Ä','Ö'};
+            int sum = 0;
 
             foreach(char x in userInput)
             {
-                if (x == arr1[])
-                    i++;
-                    
-              
-                
+                for(int i = 0; i < arr1.Length;i++)
+                {
+                    if (x == arr1[i])
+                    {
+                        sum++;
+                    }
+                }
             }
-
+            Console.WriteLine($"Syötit sanan {userInput}, siinä on vokaaleja {sum}.");
             Console.ReadKey();
         } 
     }
